bd5e9b8991dea19bc00c1780803531bfffb22561
naissustech-github <96209799+naissustech-github@users.noreply.github.com>
2021-12-24 13:39:18 +0100

    delete safari controller

--- src/cordova/cordova-browser.ts
+++ src/cordova/cordova-browser.ts
@@ -13,43 +13,21 @@ export class CordovaBrowser extends Browser {
 
     public async  closeWindow(): Promise<void> {
         await CordovaDocument.ready();
-
-        if(await SafariViewController.isAvailable()){
-            try{SafariViewController.hide(); }catch{}  
-        }else{
             if(this.inAppBrowserRef != undefined)
                 this.inAppBrowserRef.close(); 
-        } 
     }
 
     public async showWindow(url: string) : Promise<string | undefined> {
         await CordovaDocument.ready();
-
-        if(await SafariViewController.isAvailable()){
-            let optionSafari: any = {
-                url: url,    
-                showDefaultShareMenuItem: false,
-                toolbarColor: '#ffffff'
-            }
-            SafariViewController.show(optionSafari).subscribe((result : any) => {
-                if (result.event === 'closed') {
-                   this.onCloseFunction();
-                }
-            });
-        }else{
             let options: any = {
                 location: 'no',
                 zoom: 'no',
                 clearcache: 'yes',
                 clearsessioncache: 'yes',     
             }
-    
             this.inAppBrowserRef = InAppBrowser.create(url, '_self', options);
-
             if(this.inAppBrowserRef != undefined)
-                this.inAppBrowserRef.on('exit').subscribe(() => this.onCloseFunction());
-        
-        }
+                this.inAppBrowserRef.on('exit').subscribe(() => this.onCloseFunction());       
         return;
     }
         
